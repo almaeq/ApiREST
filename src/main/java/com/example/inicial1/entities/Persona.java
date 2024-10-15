12749e86fff37480561815a9630f5a02b9875bfb
almaeq <alma.quinterosquiroga@gmail.com>
2024-10-14 21:01:15 -0300

    Api

---
+++ src/main/java/com/example/inicial1/entities/Persona.java
@@ -0,0 +1,30 @@
+package com.example.inicial1.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import org.hibernate.envers.Audited;
+
+import java.io.Serializable;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@ToString
+@Builder
+@Audited
+public class Persona extends Base {
+
+    @Column(name = "nombre")
+    private String nombre;
+    @Column(name = "apellido")
+    private String apellido;
+    @Column(name = "dni")
+    private int dni;
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "fk_domicilio")
+    private Domicilio domicilio;
+}
+
